69bf611df2341e16fb132a2d9a90b86fb16fcd9d
rashedmir <rashedmir89@gmail.com>
2022-11-10 21:58:47 +0600

    jwt function added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { ObjectID, ObjectId } = require('bson');
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
@@ -16,10 +18,33 @@ app.get('/', (req, res) => {
 const uri = "mongodb+srv://service_review_user:qi3WWtiWLGfFpwM9@cluster0.klpdq3q.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    // console.log(req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decode) {
+        if (err) {
+            res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const userCollection = client.db('serviceReview').collection('category');
         const userReview = client.db('serviceReview').collection('review');
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
 
         app.get('/category', async (req, res) => {
             const query = {};
@@ -27,7 +52,7 @@ async function run() {
             const category = await cursor.toArray();
             res.send(category)
         })
-        
+
         app.get('/category/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -42,6 +67,7 @@ async function run() {
         })
 
         app.get('/review/:id', async (req, res) => {
+
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const review = await userReview.findOne(query);
@@ -69,7 +95,7 @@ async function run() {
             const review = await cursor.toArray();
             res.send(review)
         })
-        
+
         app.post('/review', async (req, res) => {
             const review = req.body;
             const result = await userReview.insertOne(review, new Date());
